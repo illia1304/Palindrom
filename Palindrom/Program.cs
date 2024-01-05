93dc35d77a83f0319370a31b5f11251fee874385
Illia <1304illia@gmail.com>
2024-01-05 20:58:57 +0200

    Add project files.

---
+++ Palindrom/Program.cs
@@ -0,0 +1,14 @@
+﻿int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(isPalindrom(x));
+
+bool isPalindrom(int number)
+{
+    string s = number.ToString();
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (s[i] != s[s.Length - 1 - i])
+            return false;
+    }
+    return true;
+}
